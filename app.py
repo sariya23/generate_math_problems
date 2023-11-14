3b7087034567b081a98e306203e891afa31e50af
nikita <dayzwaitik123@yandex.ru>
2023-11-14 16:08:07 +0300

    Add new routes

--- app.py
+++ app.py
@@ -10,14 +10,24 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/school_math')
-def school_math():
-    return 'math'
-
-
 @app.route('/theory')
 def theory():
     return 'Integrals'
+
+
+@app.route('/school_math/linear_equations')
+def linear_equations():
+    return 'math'
+
+
+@app.route('/school_math/quadratic_equations')
+def linear_equations():
+    return 'math'
+
+
+@app.route('/school_math/systems_of_linear_equations')
+def linear_equations():
+    return 'math'
 
 
 @app.route('/higher_math/integrals')
@@ -28,6 +38,7 @@ def integrals():
 @app.route('/higher_math/derivatives')
 def derivatives():
     pass
+
 
 @app.route('/higher_math/matrices')
 def matrices():
