d8b5f52572810bced4fd2ec501449631a236e7b9
nikita <dayzwaitik123@yandex.ru>
2023-11-19 21:26:49 +0300

    Fix download file

--- static/js/sendFileFormatData.js
+++ static/js/sendFileFormatData.js
@@ -16,35 +16,25 @@ document.addEventListener('DOMContentLoaded', function() {
             body: JSON.stringify(formData),
         })
         .then(function(response) {
-            // Проверяем, что ответ имеет успешный статус
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+            if (response.ok) {
+                console.log(response)
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch file information');
             }
-
-            // Получаем объект Blob из ответа
-            return response.blob();
         })
-        .then(function(blob) {
-            // Создаем URL для Blob
-            var blobUrl = URL.createObjectURL(blob);
-
-            // Создаем ссылку
-            var downloadLink = document.createElement('a');
-            downloadLink.href = blobUrl;
-            downloadLink.download = 'integrals.tex';  // замените на реальное имя и расширение файла
-            document.body.appendChild(downloadLink);
-
-            // Программно вызываем клик по ссылке
-            downloadLink.click();
-
-            // Удаляем ссылку из документа
-            document.body.removeChild(downloadLink);
-
-            // Освобождаем ресурсы URL
-            URL.revokeObjectURL(blobUrl);
+        .then(function(jsonData) {
+            // Создаем ссылку для скачивания файла
+            var a = document.createElement('a');
+            a.href = jsonData.path;
+            console.log(jsonData.path)
+            a.download = 'integrals.' + jsonData.extension;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
         })
         .catch(function(error) {
-            console.error('Error:', error);
+            console.log('Error:', error);
         });
     });
 });
