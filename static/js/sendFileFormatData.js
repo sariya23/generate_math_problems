25737b963b9fc7834d0275afcca59945c016a9e5
nikita <dayzwaitik123@yandex.ru>
2023-11-19 20:46:23 +0300

    Fix download file

--- static/js/sendFileFormatData.js
+++ static/js/sendFileFormatData.js
@@ -15,12 +15,36 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify(formData),
         })
-        .then(response => response.json())
         .then(function(response) {
-            console.log(response)
+            // Проверяем, что ответ имеет успешный статус
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Получаем объект Blob из ответа
+            return response.blob();
+        })
+        .then(function(blob) {
+            // Создаем URL для Blob
+            var blobUrl = URL.createObjectURL(blob);
+
+            // Создаем ссылку
+            var downloadLink = document.createElement('a');
+            downloadLink.href = blobUrl;
+            downloadLink.download = 'integrals.tex';  // замените на реальное имя и расширение файла
+            document.body.appendChild(downloadLink);
+
+            // Программно вызываем клик по ссылке
+            downloadLink.click();
+
+            // Удаляем ссылку из документа
+            document.body.removeChild(downloadLink);
+
+            // Освобождаем ресурсы URL
+            URL.revokeObjectURL(blobUrl);
         })
         .catch(function(error) {
-            console.log(`error: ${error}`)
+            console.error('Error:', error);
         });
     });
 });
