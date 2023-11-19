ac341c452f2f7f4a295e19283752818eeb31546f
nikita <dayzwaitik123@yandex.ru>
2023-11-19 19:54:37 +0300

    Change id

--- static/js/sendGenerateData.js
+++ static/js/sendGenerateData.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var dataForm = document.getElementById('dataForm');
+    var dataForm = document.getElementById('generate_data_integral_form');
 
     dataForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -22,9 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(function(response) {
             document.getElementById('status').textContent = 'Генерация выполнена успешно!';
+            console.log(response)
         })
         .catch(function(error) {
             document.getElementById('status').textContent = 'Что-то пошло не так! Проверьте формат данных в форме';
+            console.log(`error: ${error}`)
         });
     });
 });
