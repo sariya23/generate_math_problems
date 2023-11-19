423ab896e3ad8bfc8143c7a9e4a55a7af639bbf0
nikita <dayzwaitik123@yandex.ru>
2023-11-19 19:47:41 +0300

    Add file with response data

---
+++ static/js/sendForm.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dataForm = document.getElementById('dataForm');
+
+    dataForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var formData = {
+            pattern: document.getElementById('dataInput').value,
+            constants: document.getElementById('constantsInput').value,
+            bounds: document.getElementById('boundsInput').value,
+            numExpressions: document.getElementById('numExpressions').value,
+            fileFormat: document.getElementById('fileFormatSelect').value,
+        };
+
+        fetch('/generate_integrals', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(function(response) {
+            document.getElementById('status').textContent = 'Генерация выполнена успешно!';
+        })
+        .catch(function(error) {
+            document.getElementById('status').textContent = 'Что-то пошло не так! Проверьте формат данных в форме';
+        });
+    });
+});
