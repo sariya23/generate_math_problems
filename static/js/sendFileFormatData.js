13ff7d20201dd727a97df386a1b07fca48c6ec2d
nikita <dayzwaitik123@yandex.ru>
2023-12-15 00:04:42 +0300

    Delete comm

--- static/js/sendFileFormatData.js
+++ static/js/sendFileFormatData.js
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         })
         .then(function(jsonData) {
-            // Создаем ссылку для скачивания файла
             var a = document.createElement('a');
             a.href = jsonData.path;
             console.log(jsonData.path)
